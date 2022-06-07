af3fd941bda43ca5ff399082a52eb3a0fdf3b797
Hossein Hassani <hosseinhassani@users.noreply.github.com>
2022-06-07 09:27:39 +0300

    Add files via upload

---
+++ source/HH_KCL_Tbx_defs.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+
+#==============================================================================
+# # Hossein Hassani
+# Started @: 29 Apr 2016
+# Last update @: 12 May 2016
+
+
+######################################
+
+K2SDicFile = 'KurmanjiSoraniDicNew'
+S2KDicFile = 'SoraniKurmanjiDicNew'
+KurdishNameDic = 'KNM'
+KurdishNameDic_Arabic = 'KNMA'
+KurdishGazetteer = 'KurdishGazetteer'
+
+######################################
+
+# Exeception in "şari helemt" it does not work correctly.
+
+KurdishPNCompanionWordHead = [u'bajêrê', \
+                              u'mzgewtî', \
+                              u'mizgewtî', \
+                              u'parêzgaî' \
+                              u'senterî', \
+                              u'şarî', \
+                              ]
+
+######################################
+
+KurdishPNCompanionWordTail = [u'delê', u'delêt', u'derwa', u'dexwa', u'digot' \
+                              u'gut', u'gutî', u'gutîşî' \
+                              u'kirdî', \
+                              u'nasraw', u'nûsînî', \
+                              u'xwardî' \
+                              ]
+
+######################################
+
+KurdishSalutationHead = [u'd.', u'dade', u'hacî', u'kak', u'kake', \
+                         u'mamosta', u'mela', u'p.d.', u'şêx', \
+                         u'westa', u'xatûn']
+
+######################################
+
+KurdishSalutationTail = [u'aga', u'axa', u'beg', u'begî', u'xatûn', u'xanim']
+
+######################################
